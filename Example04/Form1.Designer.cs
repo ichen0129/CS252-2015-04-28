f003af1c1d1ad3072bedded91a1d77365c7d2420
ichen0129 <ichen.lo@gmail.com>
2015-04-29 00:29:22 +0800

    add Example04
    計算1~X間所有奇數和

---
+++ Example04/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Example04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.Calculatebutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input a number";
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberTextBox.Location = new System.Drawing.Point(139, 33);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(100, 22);
+            this.numberTextBox.TabIndex = 1;
+            this.numberTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numberTextBox_KeyPress);
+            // 
+            // Calculatebutton
+            // 
+            this.Calculatebutton.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Calculatebutton.Location = new System.Drawing.Point(70, 105);
+            this.Calculatebutton.Name = "Calculatebutton";
+            this.Calculatebutton.Size = new System.Drawing.Size(143, 42);
+            this.Calculatebutton.TabIndex = 2;
+            this.Calculatebutton.Text = "Calculate";
+            this.Calculatebutton.UseVisualStyleBackColor = true;
+            this.Calculatebutton.Click += new System.EventHandler(this.Calculatebutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 187);
+            this.Controls.Add(this.Calculatebutton);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calculate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Button Calculatebutton;
+    }
+}
+
---
+++ Example04/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculatebutton_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(numberTextBox.Text);
+            int sum = 0;
+            for (int i = 0; i < number; i++)
+            {
+                if (i % 2 != 0)
+                    sum += i;
+            }
+            MessageBox.Show(string.Format("Total ={0}", sum));
+        }
+
+        private void numberTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(e.KeyChar >= 48 && e.KeyChar <= 57) && e.KeyChar != '\b')
+            {
+                MessageBox.Show("Please input a number.");
+                e.Handled = true;
+            }
+        }
+    }
+}
