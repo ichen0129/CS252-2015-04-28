d5ac040aa0bd856af0eb6ebc02f16156e6f2914c
ichen0129 <ichen.lo@gmail.com>
2015-04-28 21:32:54 +0800

    2015-04-28

---
+++ Example01/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Program
+    {
+        private static int 我簽的號碼 = 0;
+
+        private static void Main(string[] args)
+        {
+            人生模擬器();
+        }
+
+        public static void 人生模擬器()
+        {
+            買樂透();
+            while (!中獎了())
+            {
+                買樂透();
+                if (中獎了())
+                {
+                    Console.WriteLine("YA!下班");
+                    break;
+                }
+                Console.WriteLine("繼續買，繼續上班吧");
+            }
+
+            //if (中獎了())
+            //    Console.WriteLine("YA!下班");
+            //else
+            //    Console.WriteLine("繼續買，繼續上班吧");
+        }
+
+        public static void 買樂透()
+        {
+            //亂數產生器
+            Random random = new Random();
+            //int number = random.Next(1, 1001);
+            我簽的號碼 = random.Next(1, 1001);
+        }
+
+        public static bool 中獎了()
+        {
+            //亂數產生器
+            Random random = new Random();
+            int 中獎號碼 = random.Next(1, 1001);
+            return 我簽的號碼 == 中獎號碼;
+        }
+    }
+}
---
+++ Example02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example02
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("(1)剪刀、(2)石頭、(3)布 請出拳 ...");
+
+            int userInput = int.Parse(Console.ReadLine());
+
+            Random random = new Random();
+            int pc = random.Next(1, 4);
+
+            if (pc == userInput)
+                Console.WriteLine("平手");
+            else if (pc == 1 && userInput == 2) //pc=剪刀、user=石頭
+                Console.WriteLine("你贏了!");
+            else if (pc == 1 && userInput == 3) //pc=剪刀、user=布
+                Console.WriteLine("PC贏了");
+            else if (pc == 2 && userInput == 1) //pc=石頭、user=剪刀
+                Console.WriteLine("PC贏了");
+            else if (pc == 2 && userInput == 3) //pc=石頭、user=布
+                Console.WriteLine("你贏了");
+            else if (pc == 3 && userInput == 1) //pc=布、user=剪刀
+                Console.WriteLine("你贏了");
+            else if (pc == 3 && userInput == 2) //pc=布、user=石頭
+                Console.WriteLine("你贏了");
+        }
+    }
+}
