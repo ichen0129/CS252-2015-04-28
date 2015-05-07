c9bd495c84ef88b4a4ab38222459c61f5fd1befa
ichen0129 <ichen.lo@gmail.com>
2015-05-07 22:49:49 +0800

    guess number

---
+++ GuessNumber/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace GuessNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultTextBox = new System.Windows.Forms.RichTextBox();
+            this.guessButton = new System.Windows.Forms.Button();
+            this.ansTextBox = new System.Windows.Forms.TextBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultTextBox.Location = new System.Drawing.Point(12, 51);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(209, 185);
+            this.resultTextBox.TabIndex = 0;
+            this.resultTextBox.Text = "";
+            // 
+            // guessButton
+            // 
+            this.guessButton.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessButton.Location = new System.Drawing.Point(146, 22);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(75, 23);
+            this.guessButton.TabIndex = 1;
+            this.guessButton.Text = "Guess";
+            this.guessButton.UseVisualStyleBackColor = true;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click);
+            // 
+            // ansTextBox
+            // 
+            this.ansTextBox.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ansTextBox.Location = new System.Drawing.Point(12, 22);
+            this.ansTextBox.Name = "ansTextBox";
+            this.ansTextBox.Size = new System.Drawing.Size(128, 22);
+            this.ansTextBox.TabIndex = 2;
+            this.ansTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ansTextBox_KeyPress);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(88, 246);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 16);
+            this.statusLabel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 271);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.ansTextBox);
+            this.Controls.Add(this.guessButton);
+            this.Controls.Add(this.resultTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox resultTextBox;
+        private System.Windows.Forms.Button guessButton;
+        private System.Windows.Forms.TextBox ansTextBox;
+        private System.Windows.Forms.Label statusLabel;
+
+    }
+}
+
---
+++ GuessNumber/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessNumber
+{
+    public partial class Form1 : Form
+    {
+        private int[] guess = new int[4];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            statusLabel.Text = "";
+            int number;
+
+            Random random = new Random();
+            number = 0;
+            while (number < 4)
+            {
+                guess[number] = random.Next(0, 10);
+                for (int i = 0; i < number; i++)
+                {
+                    if (guess[number] == guess[i])
+                        number--;
+                }
+                number++;
+            }
+            resultTextBox.Text = "Start\r\n";
+        }
+
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+            int countA = 0, countB = 0;
+            int[] readbuffer = new int[4];
+
+            if (ansTextBox.Text.Length < 4 || ansTextBox.Text.Length > 4)
+            {
+                MessageBox.Show("Please enter a 4 digit number");
+                return;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                readbuffer[i] = int.Parse(ansTextBox.Text.Substring(i, 1));
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (guess[i] == readbuffer[j])
+                    {
+                        if (i == j)
+                            countA++;
+                        else
+                            countB++;
+                    }
+                }
+            }
+            if (countA == 4)
+                resultTextBox.Text += "Right number !";
+            else
+                resultTextBox.Text += "Wrong... " + ansTextBox.Text + " " + countA + "A" + countB + "B" + "\r\n";
+
+            ansTextBox.Text = "";
+        }
+
+        private void ansTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(e.KeyChar >= 48 && e.KeyChar <= 57) && e.KeyChar != '\b')
+            {
+                MessageBox.Show("Please input a number.");
+                e.Handled = true;
+            }
+        }
+    }
+}
